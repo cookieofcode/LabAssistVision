d75d5f0715050256405b4a82ce2661e757382930
cookieofcode <cookieofcode@gmail.com>
2021-01-27 23:30:54 +0100

    Add comments to ICamera

--- Assets/LabVision/Camera/ICamera.cs
+++ Assets/LabVision/Camera/ICamera.cs
@@ -7,14 +7,46 @@ using JetBrains.Annotations;
 
 namespace LabVision
 {
+    /// <summary>
+    /// A camera providing video frames. The resolution (width and height) are not initialized until the camera is started.
+    /// Listen to the <see cref="CameraInitialized"/> event for resolution and color format changes. Ensure to call <see cref="Initialize"/> before
+    /// <see cref="StartCapture"/>. The event <see cref="FrameArrived"/> is invoked on each frame providing the <see cref="CameraFrame"/>.
+    /// </summary>
     interface ICamera
     {
+        /// <summary>
+        /// The width of the initialized camera. Value is not set until <see cref="Initialize"/> is complete.
+        /// </summary>
         int FrameWidth { get; }
+
+        /// <summary>
+        /// The height of the initialized camera. Value is not set until <see cref="Initialize"/> is complete.
+        /// </summary>
         int FrameHeight { get; }
+
+        /// <summary>
+        /// Initializes the camera including <see cref="FrameWidth"/> and <see cref="FrameHeight"/>.
+        /// </summary>
         Task<bool> Initialize();
+
+        /// <summary>
+        /// Starts to capture frames, the frames can be received listening to the <see cref="FrameArrived"/> event.
+        /// </summary>
         Task<bool> StartCapture();
+
+        /// <summary>
+        /// Stops capturing frames.
+        /// </summary>
         Task<bool> StopCapture();
+
+        /// <summary>
+        /// Invoked on each frame that is captured.
+        /// </summary>
         event EventHandler<FrameArrivedEventArgs> FrameArrived;
+
+        /// <summary>
+        /// Invoked after the camera is initialized using <see cref="Initialize"/>.
+        /// </summary>
         event EventHandler<CameraInitializedEventArgs> CameraInitialized;
     }
 
