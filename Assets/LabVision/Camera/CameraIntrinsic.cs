5f604d5d372de75ad334096622433481e9d02d65
cookieofcode <cookieofcode@gmail.com>
2021-01-27 22:52:59 +0100

    Refactor CameraIntrinsic

--- Assets/LabVision/Camera/CameraIntrinsic.cs
+++ Assets/LabVision/Camera/CameraIntrinsic.cs
@@ -14,59 +14,59 @@ namespace LabVision
 {
     /// <summary>
     /// Contains information on camera intrinsic parameters.
-    /// Note: This class wraps logic in Windows.Media.Devices.Core.CameraIntrinsics for use in Unity.
+    /// Note: This class wraps logic from Windows.Media.Devices.Core.CameraIntrinsics to use in Unity.
     /// </summary>
     [Serializable]
     public class CameraIntrinsic
     {
 #if ENABLE_WINMD_SUPPORT
+        /// <summary>
+        /// Holds the <see cref="CameraIntrinsic"/> of Windows UWP to use at <see cref="UnprojectAtUnitDepth"/>.
+        /// </summary>
         // TODO: https://stackoverflow.com/questions/51272055/opencv-unproject-2d-points-to-3d-with-known-depth-z
-        [CanBeNull] public Windows.Media.Devices.Core.CameraIntrinsics WindowsCameraIntrinsics;
+        [CanBeNull] public readonly Windows.Media.Devices.Core.CameraIntrinsics WindowsCameraIntrinsics;
 #endif
 
         /// <summary>
         /// Gets the focal length of the camera.
         /// </summary>
-        public Vector2 focalLength;
+        public readonly Vector2 FocalLength;
 
         /// <summary>
         /// Gets the image height of the camera, in pixels.
         /// </summary>
-        public uint imageHeight;
+        public readonly uint ImageHeight;
 
         /// <summary>
         /// Gets the image width of the camera, in pixels.
         /// </summary>
-        public uint imageWidth;
+        public readonly uint ImageWidth;
 
         /// <summary>
         /// Gets the principal point of the camera.
         /// </summary>
-        public Vector2 principalPoint;
+        public readonly Vector2 PrincipalPoint;
 
         /// <summary>
         /// Gets the radial distortion coefficient of the camera.
         /// </summary>
-        public Vector3 radialDistortion;
+        public readonly Vector3 RadialDistortion;
 
         /// <summary>
         /// Gets the tangential distortion coefficient of the camera.
         /// </summary>
-        public Vector2 tangentialDistortion;
+        public readonly Vector2 TangentialDistortion;
 
         /// <summary>
-        ///     Gets a matrix that transforms a 3D point to video frame pixel coordinates without
-        ///     compensating for the distortion model of the camera.The 2D point resulting from
-        ///     this transformation will not accurately map to the pixel coordinate in a video
-        ///     frame unless the app applies its own distortion compensation.This is useful
-        ///     for apps that choose to implement GPU-based distortion compensation instead of
-        ///     using UndistortPoint, which uses the CPU to compute the distortion compensation.
+        /// Gets a matrix that transforms a 3D point to video frame pixel coordinates without
+        /// compensating for the distortion model of the camera.The 2D point resulting from
+        /// this transformation will not accurately map to the pixel coordinate in a video
+        /// frame unless the app applies its own distortion compensation. This is useful
+        /// for apps that choose to implement GPU-based distortion compensation instead of
+        /// using UndistortPoint, which uses the CPU to compute the distortion compensation.
         /// </summary>
-        public Matrix4x4 undistortedProjectionTransform;
+        public readonly Matrix4x4 UndistortedProjectionTransform;
 
-        /// <summary>
-        /// CameraIntrinsics constructor
-        /// </summary>
         /// <param name="focalLength">focal length for the camera</param>
         /// <param name="imageWidth">image width in pixels</param>
         /// <param name="imageHeight">image height in pixels</param>
@@ -83,58 +83,69 @@ namespace LabVision
             Vector2 tangentialDistortion,
             Matrix4x4 undistortedProjectionTransform)
         {
-            this.focalLength = focalLength;
-            this.imageWidth = imageWidth;
-            this.imageHeight = imageHeight;
-            this.principalPoint = principalPoint;
-            this.radialDistortion = radialDistortion;
-            this.tangentialDistortion = tangentialDistortion;
-            this.undistortedProjectionTransform = undistortedProjectionTransform;
+            FocalLength = focalLength;
+            ImageWidth = imageWidth;
+            ImageHeight = imageHeight;
+            PrincipalPoint = principalPoint;
+            RadialDistortion = radialDistortion;
+            TangentialDistortion = tangentialDistortion;
+            UndistortedProjectionTransform = undistortedProjectionTransform;
         }
 
+        /// <summary>
+        /// Constructor using default values.
+        /// </summary>
+        [Obsolete]
         public CameraIntrinsic()
         {
-            focalLength = Vector2.zero;
-            imageWidth = 0;
-            imageHeight = 0;
-            principalPoint = Vector2.zero;
-            radialDistortion = Vector3.zero;
-            tangentialDistortion = Vector2.zero;
-            undistortedProjectionTransform = Matrix4x4.identity;
+            FocalLength = Vector2.zero;
+            ImageWidth = 0;
+            ImageHeight = 0;
+            PrincipalPoint = Vector2.zero;
+            RadialDistortion = Vector3.zero;
+            TangentialDistortion = Vector2.zero;
+            UndistortedProjectionTransform = Matrix4x4.identity;
         }
 
+        /// <summary>
+        /// Constructor using default values except the project transformation.
+        /// </summary>
+        /// <param name="undistortedProjectionTransform">Undistorted projection transform for the camera</param>
         public CameraIntrinsic(Matrix4x4 undistortedProjectionTransform)
         {
-            focalLength = Vector2.zero;
-            imageWidth = 0;
-            imageHeight = 0;
-            principalPoint = Vector2.zero;
-            radialDistortion = Vector3.zero;
-            tangentialDistortion = Vector2.zero;
-            this.undistortedProjectionTransform = undistortedProjectionTransform;
+            FocalLength = Vector2.zero;
+            ImageWidth = 0;
+            ImageHeight = 0;
+            PrincipalPoint = Vector2.zero;
+            RadialDistortion = Vector3.zero;
+            TangentialDistortion = Vector2.zero;
+            UndistortedProjectionTransform = undistortedProjectionTransform;
         }
 
+        /// <summary>
+        /// Copy Constructor (shallow). On Windows UWP, values of the original camera intrinsics are used.
+        /// </summary>
         public CameraIntrinsic([NotNull] CameraIntrinsic intrinsic)
         {
             if (intrinsic == null) throw new ArgumentNullException(nameof(intrinsic));
 #if ENABLE_WINMD_SUPPORT
             WindowsCameraIntrinsics = intrinsic.WindowsCameraIntrinsics;
             if (WindowsCameraIntrinsics == null) throw new NullReferenceException(nameof(WindowsCameraIntrinsics));
-            focalLength = new Vector2(WindowsCameraIntrinsics.FocalLength.X, WindowsCameraIntrinsics.FocalLength.Y);
-            imageWidth = WindowsCameraIntrinsics.ImageWidth;
-            imageHeight = WindowsCameraIntrinsics.ImageHeight;
-            principalPoint = new Vector2(WindowsCameraIntrinsics.PrincipalPoint.X, WindowsCameraIntrinsics.PrincipalPoint.Y);
-            radialDistortion = new Vector3(WindowsCameraIntrinsics.RadialDistortion.X, WindowsCameraIntrinsics.RadialDistortion.Y, WindowsCameraIntrinsics.RadialDistortion.Z);
-            tangentialDistortion = new Vector2(WindowsCameraIntrinsics.TangentialDistortion.X, WindowsCameraIntrinsics.TangentialDistortion.Y);
-            undistortedProjectionTransform = WindowsCameraIntrinsics.UndistortedProjectionTransform.ToUnity();
+            FocalLength = new Vector2(WindowsCameraIntrinsics.FocalLength.X, WindowsCameraIntrinsics.FocalLength.Y);
+            ImageWidth = WindowsCameraIntrinsics.ImageWidth;
+            ImageHeight = WindowsCameraIntrinsics.ImageHeight;
+            PrincipalPoint = new Vector2(WindowsCameraIntrinsics.PrincipalPoint.X, WindowsCameraIntrinsics.PrincipalPoint.Y);
+            RadialDistortion = new Vector3(WindowsCameraIntrinsics.RadialDistortion.X, WindowsCameraIntrinsics.RadialDistortion.Y, WindowsCameraIntrinsics.RadialDistortion.Z);
+            TangentialDistortion = new Vector2(WindowsCameraIntrinsics.TangentialDistortion.X, WindowsCameraIntrinsics.TangentialDistortion.Y);
+            UndistortedProjectionTransform = WindowsCameraIntrinsics.UndistortedProjectionTransform.ToUnity();
 #else
-            focalLength = intrinsic.focalLength;
-            imageWidth = intrinsic.imageWidth;
-            imageHeight = intrinsic.imageHeight;
-            principalPoint = intrinsic.principalPoint;
-            radialDistortion = intrinsic.radialDistortion;
-            tangentialDistortion = intrinsic.tangentialDistortion;
-            undistortedProjectionTransform = intrinsic.undistortedProjectionTransform;
+            FocalLength = intrinsic.FocalLength;
+            ImageWidth = intrinsic.ImageWidth;
+            ImageHeight = intrinsic.ImageHeight;
+            PrincipalPoint = intrinsic.PrincipalPoint;
+            RadialDistortion = intrinsic.RadialDistortion;
+            TangentialDistortion = intrinsic.TangentialDistortion;
+            UndistortedProjectionTransform = intrinsic.UndistortedProjectionTransform;
 #endif
         }
 
@@ -142,20 +153,20 @@ namespace LabVision
         public CameraIntrinsic([NotNull] Windows.Media.Devices.Core.CameraIntrinsics cameraIntrinsics)
         {
             if (cameraIntrinsics == null) throw new ArgumentNullException(nameof(cameraIntrinsics));
-            focalLength = new Vector2(cameraIntrinsics.FocalLength.X, cameraIntrinsics.FocalLength.Y);
-            imageWidth = cameraIntrinsics.ImageWidth;
-            imageHeight = cameraIntrinsics.ImageHeight;
-            principalPoint = new Vector2(cameraIntrinsics.PrincipalPoint.X, cameraIntrinsics.PrincipalPoint.Y);
-            radialDistortion = new Vector3(cameraIntrinsics.RadialDistortion.X, cameraIntrinsics.RadialDistortion.Y, cameraIntrinsics.RadialDistortion.Z);
-            tangentialDistortion = new Vector2(cameraIntrinsics.TangentialDistortion.X, cameraIntrinsics.TangentialDistortion.Y);
-            undistortedProjectionTransform = cameraIntrinsics.UndistortedProjectionTransform.ToUnity();
+            FocalLength = new Vector2(cameraIntrinsics.FocalLength.X, cameraIntrinsics.FocalLength.Y);
+            ImageWidth = cameraIntrinsics.ImageWidth;
+            ImageHeight = cameraIntrinsics.ImageHeight;
+            PrincipalPoint = new Vector2(cameraIntrinsics.PrincipalPoint.X, cameraIntrinsics.PrincipalPoint.Y);
+            RadialDistortion = new Vector3(cameraIntrinsics.RadialDistortion.X, cameraIntrinsics.RadialDistortion.Y, cameraIntrinsics.RadialDistortion.Z);
+            TangentialDistortion = new Vector2(cameraIntrinsics.TangentialDistortion.X, cameraIntrinsics.TangentialDistortion.Y);
+            UndistortedProjectionTransform = cameraIntrinsics.UndistortedProjectionTransform.ToUnity();
             WindowsCameraIntrinsics = cameraIntrinsics;
         }
         
         /// <summary>
         /// Unprojects pixel coordinates into a camera space ray from the camera origin, expressed as a X, Y coordinates on a plane one meter from the camera.
-        /// Note: param needs to be point, windows has different coordinates
         /// </summary>
+        /// <param name="pixelCoordinate">The point to unproject. Points in Windows UWP use a different coordinate system than OpenCV</param>
         public Vector2 UnprojectAtUnitDepth(Windows.Foundation.Point pixelCoordinate)
         {
             if (WindowsCameraIntrinsics == null) throw new NotImplementedException("Unprojection without UWP is not implemented yet.");
@@ -166,7 +177,7 @@ namespace LabVision
 
         public override string ToString()
         {
-            return $"Focal Length:{focalLength.ToString("G4")}, Principal Point:{principalPoint.ToString("G4")}, Image Width:{imageWidth:G4}, Image Height:{imageHeight:G4}, Radial Distortion:{radialDistortion.ToString("G4")}, Tangential Distortion:{tangentialDistortion.ToString("G4")}";
+            return $"Focal Length:{FocalLength.ToString("G4")}, Principal Point:{PrincipalPoint.ToString("G4")}, Image Width:{ImageWidth:G4}, Image Height:{ImageHeight:G4}, Radial Distortion:{RadialDistortion.ToString("G4")}, Tangential Distortion:{TangentialDistortion.ToString("G4")}";
         }
     }
 }
