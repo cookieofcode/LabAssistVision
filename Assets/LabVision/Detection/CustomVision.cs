1589e55aa0504b7124b1eb7fb6da09d00b777c54
cookieofcode <cookieofcode@gmail.com>
2021-01-28 14:23:35 +0100

    Refactor Custom Vision

--- Assets/LabVision/Detection/CustomVision.cs
+++ Assets/LabVision/Detection/CustomVision.cs
@@ -18,29 +18,27 @@ namespace LabVision.Detection
         [NotNull] private readonly Logger _logger;
         [NotNull] private readonly string _predictionKey;
         [NotNull] private readonly string _predictionEndpoint;
-
+        [NotNull] private readonly Stopwatch _stopwatch;
         public CustomVision([NotNull] string predictionApi, [NotNull] string predictionKey)
         {
             if (predictionApi == null) throw new ArgumentNullException(nameof(predictionApi));
             if (predictionKey == null) throw new ArgumentNullException(nameof(predictionKey));
-
-            _logger = new Logger(new LogHandler());
-            Assert.IsNotNull(_logger, "_logger != null");
-
             _predictionEndpoint = predictionApi;
             _predictionKey = predictionKey;
+            _logger = new Logger(new LogHandler());
+            Assert.IsNotNull(_logger, "_logger != null");
+            _stopwatch = new Stopwatch();
         }
 
         /// <summary>
-        /// Call the Computer Vision Service to submit the image.
+        /// Detects objects using the custom vision cloud service.
         /// <param name="frame">The current video frame</param>
         /// </summary>
         [ItemNotNull]
         public async Task<List<DetectedObject>> DetectAsync(CameraFrame frame)
         {
             if (frame == null) throw new ArgumentNullException(nameof(frame));
-            const string jpgExtension = ".jpg";
-            byte[] jpgEncoded = frame.EncodeImage(jpgExtension);
+            byte[] jpgEncoded = frame.EncodeImage();
 
             using (HttpClient client = new HttpClient())
             {
@@ -49,12 +47,12 @@ namespace LabVision.Detection
                 using (var content = new ByteArrayContent(jpgEncoded))
                 {
                     content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
-                    Stopwatch stopwatch = new Stopwatch();
                     _logger.Log("Starting CustomVision Request");
-                    stopwatch.Start();
+                    _stopwatch.Reset();
+                    _stopwatch.Start();
                     response = await client.PostAsync(_predictionEndpoint, content);
-                    stopwatch.Stop();
-                    _logger.Log($"CustomVision request took {stopwatch.Elapsed.Milliseconds} milliseconds");
+                    _stopwatch.Stop();
+                    _logger.Log($"CustomVision request took {_stopwatch.Elapsed.Milliseconds} milliseconds");
                 }
 
                 string jsonResponse = await response.Content.ReadAsStringAsync();
@@ -87,11 +85,15 @@ namespace LabVision.Detection
             return new DetectedObject(rect, label, probability, frame);
         }
 
+        /// <summary>
+        /// Converts the <see cref="BoundingBox"/> received from Custom Vision to an OpenCV rectangle.
+        /// <seealso href="https://stackoverflow.com/questions/50794707/how-to-use-azure-custom-vision-service-response-boundingbox-to-plot-shape"/>
+        /// </summary>
+        /// <returns></returns>
         [NotNull]
         private Rect2d Convert([NotNull] BoundingBox boundingBox, int width, int height)
         {
             if (boundingBox == null) throw new ArgumentNullException(nameof(boundingBox));
-            // https://stackoverflow.com/questions/50794707/how-to-use-azure-custom-vision-service-response-boundingbox-to-plot-shape
             // BoundingBox values are in percent of the image original size
             return new Rect2d(boundingBox.left * width, boundingBox.top * height, boundingBox.width * width, boundingBox.height * height);
         }
